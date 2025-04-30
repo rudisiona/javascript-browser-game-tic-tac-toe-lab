aa3da9dbacd30262e974e9efdbd75266cf47501d
rudisiona <rsmageo@gmail.com>
2025-04-29 20:25:16 -0700

    left off at checkForWinner

--- js/app.js
+++ js/app.js
@@ -1,21 +1,86 @@
-body {
-    background-color: gainsboro;
-    /* Use a system font, if none are available use an available sans-sarif */
-    font-family: system-ui, sans-serif;
-    margin: 0;
+/*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board;
+let turn;
+let winner;
+let tie;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const squareEls = document.querySelectorAll('.sqr')
+// console.log(squareEls)
+
+const messageEl = document.querySelector('#message')
+// console.log(messageEl)
+const boardEl = document.querySelector('.board'); 
+
+/*-------------------------------- Functions --------------------------------*/
+
+const init =  () => {
+  board = ['', '', '', '', '', '', '', '', ''];
+  turn = 'X';
+  winner = false;
+  tie = false;
+  render()
+}
+const render = () => {
+updateBoard()
+updateMessage()
+}
+
+const updateBoard = () => {
+  board.forEach((square, idx) => {
+    squareEls[idx].textContent = square;
+  })
+}
+const updateMessage = () => {
+  if (winner === false && tie === false) {
+  messageEl.textContent = `${turn}'s turn`
+  } else if (winner === false && tie === true) {
+  messageEl.textContent = `well you tied`
+  } else {
+    messageEl.textContent = ` nice you won`
   }
+}
+const handleClick = (event) => {
+  const clickedSquare = event.target;
+  const squareIndex = event.target.id
+  if (!clickedSquare.classList.contains('sqr')) return;
+  if (board[squareIndex] === 'X' || board[squareIndex] === 'O') return;
+  if (winner) return;
+  placePiece(squareIndex);
+  // render();
+ 
+}
+
+const placePiece = (index) => {
+  board[index] = turn
+  console.log(board)
+}
+
+const checkForWinner = () => {
   
-  .board {
-    display: flex;
-    flex-wrap: wrap;
-    width: 325px;
-  }
-  
-  .sqr {
-    width: 30%;
-    aspect-ratio: 1/1;
-    border: 3px solid black;
-    font-size: 64px;
-    text-align: center;
-  }
+}
+
+window.onload = init;
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+boardEl.addEventListener('click', handleClick)
+
+
   
